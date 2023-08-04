eda37b67fbd18e7bc4895126f4a5184cd05fd0f8
Samuel Maboza <m.hondi.samuel@gmail.com>
2023-08-04 22:40:53 +0200

    Initial Commit

---
+++ anytime_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anytime_ios
+//
+//  Created by samuel maboza hondi on 04/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ anytime_ios/anytime_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  anytime_iosApp.swift
+//  anytime_ios
+//
+//  Created by samuel maboza hondi on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct anytime_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
